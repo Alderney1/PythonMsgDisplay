8e4f13948573d6b2fc6c90180ab8b856d5eb4f7f
Mats Larsen <you@example.com>
2014-09-26 23:10:50 +0200

    Change that the msg is in a separated file or files.

--- code/msg.py
+++ code/msg.py
@@ -1,25 +1,42 @@
-"""
-Module for class to contain display messeges.
-"""
-__author__ = "Mats LarseN"
+#--------------------------------------------------------------------
+#Administration Details
+#--------------------------------------------------------------------
+__author__ = "Mats Larsen"
 __copyright__ = "Mats Larsen 2014"
 __credits__ = ["Mats Larsen"]
 __license__ = "GPLv3"
 __maintainer__ = "Mats Larsen"
 __email__ = "larsen.mats.87@gmail.com"
 __status__ = "Development"
+__description__ = "Module is for displaying messeges. The Class should contain all the desired display messages. The idea is that it is typically the same messages there will be displayed. It should be understand as a fast phototyping of a appluication to display common messages."
+__file__ = "msg.py"
+__class__ ="DisplayMsg"
+__dependencies__ = ["DisplayMsg"]
+#--------------------------------------------------------------------
+#Version
+#--------------------------------------------------------------------
+__version_stage__ = "Pre_alpha"
+__version_number__ = "0.1"
+__version_date__ = "20140917"
+__version_risk__ = "This current version is in Pre-alpha version, which meaning that the program can crash or perform other unrespected behavoiurs."
+__version_modification__ = "The development project has just been created."
+__version_next_update__ = "Implementation of more messeages."
+#--------------------------------------------------------------------
+#Hardware
+#--------------------------------------------------------------------
+"""
+This project is not releated to any kind of hardware, like GPIOs.
+"""
 #-----------------------------------------------------
 #Import
 #-----------------------------------------------------
 import traceback
-import numpy as np
+import os
 #--------------------------------------------------------------------
 #CONSTANTS
 #--------------------------------------------------------------------
 LOG_LEVEL = 2 # Information level
-LOG_ALWAYS = 3
-FILE = 'msg'
-CLASS = 'DisplayMsg'
+LOG_ALWAYS = 3 # Always log data
 #--------------------------------------------------------------------
 #METHODS
 #-------------------------------------------------------------------
@@ -32,34 +49,34 @@ def log(msg, log_level=LOG_LEVEL):
     """
     global LOG_LEVEL
     if log_level <= LOG_LEVEL:
-        print(str(log_level) + ' : ' + FILE + '.py::' + traceback.extract_stack()[-2][2] + ' : ' + msg)
+        print(str(log_level) + ' : ' + __file__ + '.py::' + traceback.extract_stack()[-2][2] + ' : ' + msg)
 
 class DisplayMsg(object):
     """
     This class contains all meassesges for given system.
     """
-    def __init__(self,name,log_level):
+    def __init__(self,*args,**kwargs):
         """
         The constructor for the DisplayMsg.
         """
-        self._name = name
+        #self._name = name # Name of the instance
+        print(args)
+        for a in args:
+            print('here')
+            print(a)
+           
+            
+            if os.path.exists(a):   
+                log('File exist')
+                f = open(a,'r')
+            else:
+                log('Do not exist')
+            #msg = f.readlines()
+            
+        #print(msg)
         self._msg = [] # List containing all messages
 
-        self._msg.append("The given mode was equal to the already operation mode.") #0
-
-        self._msg.append("All diodes are not blocked. Meaning that the tube is free of plastic.")#1
-
-        self._msg.append("The given operational mode is undefined acoording to the listed implemented methods. Please check the mode name and the desired implemented method for consistensis. The application will be shutted down now!!") #2
-        self._msg.append(" is initilized and ready to be used!") #3
-        self._msg.append("A detection event is succesfull added")#4
-        self._msg.append("ErrorTyping : The arguments of the name is not a String, Please change it to a String!!")#5
-
-        self._msg.append("Name Typing Error")#6
-        self._msg.append("Type Typing Error")#7
-        self._msg.append("ErrorTyping. The arguments of the type is defined, Please change it to a defined type!!")#8
-        self._msg.append("ErrorTyping. The arguments of the log_level is undefined, Please change it to a integer!!")#9
-        self._msg.append("Argument is not prensent in the method")#10
-        self._msg.append("Forfill the non present argument in the method")#11 
+   
     def getMsg(self, index):
         """
         Return a messages given by the argument(index).
